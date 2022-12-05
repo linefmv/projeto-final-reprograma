55ae444d4132c35497b5caedd1fb68c64e3df0d7
aline.viana <aline.viana@cloud2go.com.br>
2022-12-04 21:22:28 -0300

    tests: first test

--- helloWether/severeWheter/SevereWeather.js
+++ helloWether/severeWheter/SevereWeather.js
@@ -2,21 +2,21 @@ class WeatherCep {
   constructor(cep) {
     this.cep = cep;
   }
-  
-  
-  formatCep() {
-   if (this.cep > 1 ) {
-      return 'oi'
-   } else {
-      return 'tchau'
-   }
-  }
 
-  getCep() {
-    return this.cep;
+  formatCep() {
+    const checkIfHasLetterRegex = /[a-zA-Z]/g;
+    const checkIfHasLetterRegexResult = checkIfHasLetterRegex.test(this.cep) === true;
+
+    if (checkIfHasLetterRegexResult) {
+      return 'Hmm... Acho que tem uma letra no seu cep. Tente novamente, por favor.';
+    }
+    if (this.cep.length >= 9 || this.cep.length <= 7) {
+      return 'Olá, o cep deve conter oito número. Tente novamente, por favor :D';
+    }
+    else {
+      return 'CEP: ' + this.cep;
+    }
   }
-  
 }
 
-// 
 export default WeatherCep;
