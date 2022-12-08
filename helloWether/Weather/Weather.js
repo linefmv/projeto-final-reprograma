2a420b9dee18f8a55ee5b892078510566ccbcc64
aline.viana <aline.viana@cloud2go.com.br>
2022-12-08 09:32:53 -0300

    feat: get infos about weather

--- helloWether/Weather/Weather.js
+++ helloWether/Weather/Weather.js
@@ -1,5 +1,8 @@
 import fetch from 'node-fetch';
 import dotenv from "dotenv";
+import translate from "translate";
+translate.engine = "google";
+translate.key = process.env.GOOGLE_KEY;
 dotenv.config();
 
 class Weather {
@@ -8,23 +11,54 @@ class Weather {
     }
 
     async callApiTransformCityInLatAndLog() {
-        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.city}&key=${process.env.SUBSCRIPTION_GEOLOCATION_API_KEY}`);
+        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.city}&key=${process.env.GOOGLE_KEY}`);
         const data = await response.json();
-        return data
+
+        return data;
     }
 
     async callApiWheter() {
         const callFunction = await this.callApiTransformCityInLatAndLog();
-        
-        const lat = callFunction.results[0].geometry.location.lat;
-        const long = callFunction.results[0].geometry.location.lng;
 
-        const api = await fetch(`https://atlas.microsoft.com/weather/severe/alerts/json?api-version=1.0&query=${lat},${long}&subscription-key=${process.env.SUBSCRIPTION_API_KEY}`);
-        const data = await api.json();
-        return data;
+        if (callFunction.status === 'ZERO_RESULTS') {
+            console.log('Ops! Não conseguimos identificar a sua cidade, tente novamente!');
+        } else {
+            const lat = callFunction.results[0].geometry.location.lat;
+            const long = callFunction.results[0].geometry.location.lng;
+
+            const api = await fetch(`https://atlas.microsoft.com/weather/severe/alerts/json?api-version=1.0&query=${lat},${long}&language={pt-BR}&subscription-key=${process.env.MICROSOFT_KEY}`);
+            const data = await api.json();
+
+            return data;
+        }
     }
 
-}
+    async getWeather() {
 
-const weather = new Weather('Santa Catarina');
-console.log(weather.callApiWheter());
+        const getLocalWeather = await this.callApiWheter();
+
+        const newObj = {};
+        const getInfos = getLocalWeather.results.map((item) => {
+            newObj.level = item.level;
+            newObj.source = item.source;
+            newObj.description = item.description.localized;
+            newObj.city = item.alertAreas[0].name;
+            newObj.latestStatus = item.alertAreas[0].latestStatus.localized;
+            newObj.alertDetails = item.alertAreas[0].alertDetails;
+
+            return newObj;
+        });
+        const translateDescription = await translate(newObj.description, "pt");
+        const translateLevel = await translate(newObj.level, "pt");
+        const translateStautsOfAlert = await translate(newObj.latestStatus, "pt");
+
+
+        if (newObj.level) {
+            console.log('Atenção! Foi emitido um alerta', translateLevel, 'para a sua cidade:', translateDescription, '\n', '\n', newObj.alertDetails, '\n', '\n', '- Fonte: ' + newObj.source, '\n', '- Status da emissão do alerta: ' + translateStautsOfAlert);
+        } else {
+            console.log('Não foi emitido nenhum alerta para a sua cidade');
+        }
+
+        return getInfos
+    }
+}
