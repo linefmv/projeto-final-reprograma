17256b68102e57c60caa77cfc3a7eea610672b3b
aline.viana <aline.viana@cloud2go.com.br>
2022-12-08 19:58:04 -0300

    refactor: change path

--- helloWether/Weather/Weather.test.js
+++
@@ -1,8 +0,0 @@
-import Weather from "./Weather.js";
-
-describe("get city name in Google Api", () => {
-    it("should return city name", () => {
-        const city = new Weather('São Paulo');
-        expect(city.callApiTransformCityInLatAndLog()).toMatch("São Paulo");
-    });
-});
